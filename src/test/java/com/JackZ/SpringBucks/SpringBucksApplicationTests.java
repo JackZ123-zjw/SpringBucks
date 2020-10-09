ae7dc5b8d56fba79fabcb4f08a1f21d257cc2756
JINGWEN Zhuang <jxz907@cs.bham.ac.uk>
2020-10-09 08:09:07 +0800

    Initial project

---
+++ src/test/java/com/JackZ/SpringBucks/SpringBucksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JackZ.SpringBucks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBucksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
